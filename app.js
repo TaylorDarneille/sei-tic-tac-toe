ec4fe6da072962e979e4b1a6b6502e8128457987
Paola Tramontin <paolatramontin@Paolas-MacBook-Pro.local>
2020-09-15 22:55:22 -0400

    updated TTT'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// tic tac toe!
+// Tic Tac Toe!
 
 
 
@@ -8,10 +8,22 @@
 //4. Player 1 gets X  consistent
 //5. player 2 gets O   consistent
 //6. This must be consistant throughout the entire game!
-
 //7. Each player will have their own turn, player 1 will put their variable of choice on the grid, by clicking on any square of their choice that does not already have a variable on.  
 //8. Once player 1 selects their square of choice, it is now player 2 turn.
 //9. Player 2 can now select to put their varialble on the grid. They can only select a square that does not have a variable already on it.
+//10. Each player can have up to 3 turns.
+//11. Optional, maybe put an alert that says whos turn it is. Also, maybe add another alert that stops a player from cliking anything if it is not their turn.
+//12. Alert  when game ends. If there is a winner, alert the name.
 
+// How to win?
 
-// how to win?
+//1. Whichever player is able to get 3 of their variables in a row wins. Row has a flexible meaning, it can be up, down or diagnol. As long as the variables are connected to eachother.
+
+//2. Possibilty of a tie. If players are unable to create a row, the game ends in a tie. Maybe add an alert that says Tied Game.
+
+//3. I would have a score board somewhere in the page. Tic Tac Toe is a fun game & players will most likely play more than once. Make sure the scoreboard is keeping track of who is winning the game. Everytime a player wins, add +1 to the scoreboard. If it is a tie, don't add anything
+
+//AFTER GAME ENDS
+
+//1. Give the players an option to play again. Maybe set an alert and say, play again? I would also give them the option to choose another variable. Maybe they want to be X now instead of O.
+
