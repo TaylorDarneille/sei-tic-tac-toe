bd9258213a2583064942c4fa9e2f395defa3dbc5
Paola Tramontin <paolatramontin@Paolas-MacBook-Pro.local>
2020-09-15 22:54:49 -0400

    TTT

---
+++ app.js
@@ -0,0 +1,17 @@
+// tic tac toe!
+
+
+
+//1. Make a grid that is 3 by 3 
+//2. Create two different variables.
+//3. Variables should be X and O
+//4. Player 1 gets X  consistent
+//5. player 2 gets O   consistent
+//6. This must be consistant throughout the entire game!
+
+//7. Each player will have their own turn, player 1 will put their variable of choice on the grid, by clicking on any square of their choice that does not already have a variable on.  
+//8. Once player 1 selects their square of choice, it is now player 2 turn.
+//9. Player 2 can now select to put their varialble on the grid. They can only select a square that does not have a variable already on it.
+
+
+// how to win?
