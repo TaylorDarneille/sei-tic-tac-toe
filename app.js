0c6579968277c21344b47d2e7c81f35083f38b83
NatrezC <cnatrez@gmail.com>
2020-09-18 19:39:55 -0500

    Made a board, now figuring out how to make 3 by 3 grid

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const gameBoard = () => {
-    let div = document.createElement('div')
-    div.className = "board"
-    div.setAttribute('class', 'container')
+// const gameBoard = () => {
+//     let div = document.createElement('div')
+//     div.className = "board"
+//     div.setAttribute('class', 'container')
     
-    // //div.addEventListener('click', changeDiv)
-    // document.body.appendChild(div) //make div a baby of container
+//     // //div.addEventListener('click', changeDiv)
+//     // document.body.appendChild(div) //make div a baby of container
     
-}
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
     
-})
+// })
