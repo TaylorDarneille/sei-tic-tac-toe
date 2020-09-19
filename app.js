ee55261898e1ddc6ea88141a92ea1ec0e1a0ed6c
taylorschmidt <taylorhillschmidt@gmail.com>
2020-09-19 16:49:50 -0600

    fixed a bunch of code after working with lab group. need to add more edits.

--- app.js
+++ app.js
@@ -1,85 +1,98 @@
-//WHAT I NEED HELP WITH:
-// 1. Switch Player function. How to switch to the other player AFTER their turn is complete?
-// 2. Check for Winner Function - how to compare makeArrays to the winCombinations.
-// 3. How to keep a square from being replayed?
-// 4. Freeze board after a win/draw.
-// 5. Clear board with my reset button.
-// 6. Displaying whose turn it is. Is this with an alert? Do I add a header when the player switches?
 
-
-
-/////////////////variables/////////////////
-
-// const human = 'X'
-// const computer = 'O'
-let currentPlayer = 'X'
-const boxes = document.querySelectorAll('.box')
-const winCombinations = [
-    [0, 1, 2],
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [6, 4, 2],
+//create an empty array that will be filled if/when a cell is clicked. This will tie into the id of each box
+const gameSlots = ['','','','','','','','','']
+//Create the options to win
+const winningOptions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
 ]
 
+const status = document.querySelector('.status')
 
-//////////////play functions////////////////
+//player's turn function
 
-const currentMove = () => {
-     
-        if (currentPlayer === 'X') {
-            for (const box of boxes) {
-                box.addEventListener('click', () => {
-                    box.innerText= currentPlayer
-                    box.classList.add('x')
-                    switchPlayer()
-                })
-             }
-        } else if (currentPlayer === "O") {
-            let randomNum = Math.floor(Math.random() * 9)
-            let generateIDName = `#${randomNum}`
-            let computerBox = document.querySelector(generateIDName)
-            computerBox.innerText = currentPlayer
-            computerBox.classList.add('o')
-            switchPlayer()
+const playerTurn = (event) => {
+    const clickedBox = event.target
+    const boxNumber = clickedBox.id
+    if (gameSlots[boxNumber] === ''){
+        gameSlots[boxNumber] = 'X'
+        clickedBox.innerText = gameSlots[boxNumber]
+        status.innerText = ('Nice move!')
+        setTimeout(checkStatus, 1000)
+    } else if(gameSlots[boxNumber] === 'X'){
+        playerTurn()
+    }
+    setTimeout(computerTurn, 1000)
+}
+document.querySelectorAll('.box').forEach(box => box.addEventListener('click', playerTurn))
+//computer's turn
+const computerTurn = () => {
+    checkStatus()
+    let computerSlot = Math.floor(Math.random()*9)
+    let boxNumber = document.getElementById(computerSlot)
+    if (boxNumber.innerText === 'X' || boxNumber.innerText === 'O'){
+        computerTurn()
+    } else if(boxNumber.innerText === ''){
+        boxNumber.innerText = 'O'
+        gameSlots[computerSlot] = boxNumber.innerText
+        console.log(gameSlots[computerSlot])
+        console.log(gameSlots)
+        checkStatus()
+    }
+}
+  
+//winning status function
+let gameWon = false
+const checkStatus = (event) => {
+    for (let i=0; i<8; i++){
+        const winningCombo = winningOptions[i]
+        const indexOne = gameSlots[winningCombo[0]]
+        const indexTwo = gameSlots[winningCombo[1]]
+        const indexThree = gameSlots[winningCombo[2]]
+        if (indexOne === 'X' && indexTwo === 'X' && indexThree === 'X'){
+            status.innerText = 'Congratulations! You have won the game!'
+            const boxes = document.querySelectorAll('.box')
+            for(box of boxes) {
+                box.style.background = 'green'
+            }
+            gameWon === true
+            return computerTurn()
+            
+         }
+        else if (indexOne === 'O' && indexTwo === 'O' && indexThree === 'O'){
+            status.innerText = 'The computer has won the game!'
+            gameWon === true
+            const boxes = document.querySelectorAll('.box')
+            for(box of boxes) {
+                box.style.background = 'red'
+            }
+            
+        } else if (indexOne !== '' && indexTwo !== '' && indexThree !== '') {
+            const boxes = document.querySelectorAll('.box')
+            status.innerText = 'It is a tie. Play again!'
+        } else {
+           status.innerText = 'Your turn!'
+           continue
         }
-} 
+    }}
 
-// const switchPlayer = () => {
-//         if (currentPlayer === 'X') {
-//             currentPlayer = 'O'
-//          } else if (currentPlayer === 'O') {
-//         currentPlayer = 'X'
-//         }
-// }  
-
-function switchPlayer() {
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-}
-
-currentMove()
-
-
-/////////////////////winning function//////////////////////
-
-
-
-const makeArrays = () => {
-    const xClasses = document.querySelectorAll('.x')
-    console.log(xClasses)
-    const oClasses = document.querySelectorAll('.o')
-    console.log(oClasses)
-    //loop over the xClasses array
-    //loop over the winCombinations array
-    //if xClasses array has same values as the options in the winCombinations array, declare X the winner
-    //else if oClasses array has same vales as the options in the winCombinations array, declare o the winner
-    //else, declare draw (?)
-}
-
-////////////////////reset function//////////////////////
-
-//when button is clicked
-//invoke a board reset
+//restart function
+const restart = (event) => {
+        const boxes = document.querySelectorAll('.box')
+        document.querySelector('#button').addEventListener('click', ()=>{
+            for (box of boxes) {
+               box.innerText=''
+            }
+            for(let i=0;i<9; i++){
+                gameSlots[i] = ''
+            }
+            status.innerText = "Click on a box to play!"
+        })
+    }
+    document.querySelector('#button').addEventListener('click', restart)
