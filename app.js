07ad2e9f411e7610e03262e4a6daa4a854ba94bd
nichro02 <nichro02@gmail.com>
2020-09-20 15:52:58 -0700

    Cleaned up game logic

--- app.js
+++ app.js
@@ -1,104 +1,72 @@
 let currentPlayer = 'X'
 let invertedPlayer
-let playerOne = ''
-let playerTwo = ''
+let playerOne = 'Player One'
+let playerTwo = 'Player Two'
 let turnCounter = 0
 let playerOneArray = []
 let playerTwoArray = []
-//call boardRefresh function at start of game to make grid look nicer?
+let activeBoard = true
 
-const addName1 = () => {
+//Add name for player one
+const addName1 = (event) => {
     const name1 = document.querySelector('#inputBox1')
     const button1 = document.querySelector('#inputButton1')
-
-    button1.addEventListener('click', () => {
-        let user1Name = name1.value
-        //playerOne = name1.value
-        console.log(user1Name)
-        document.querySelector('#player1').innerHTML = user1Name
-        name1.remove()
-        button1.remove()
-    })
+    playerOne = name1.value
+    document.querySelector('#player1').innerHTML = playerOne
+    name1.remove()
+    button1.remove()
 }
 
-const addName2 = () => {
+//Add name for player two
+const addName2 = (event) => {
     const name2 = document.querySelector('#inputBox2')
     const button2 = document.querySelector('#inputButton2')
-
-    button2.addEventListener('click', () => {
-        let user2Name = name2.value
-        playerTwo = name2.value
-        console.log(user2Name)
-        document.querySelector('#player2').innerHTML = user2Name
-        name2.remove()
-        button2.remove()
-    })
+    playerTwo = name2.value
+    document.querySelector('#player2').innerHTML = playerTwo
+    name2.remove()
+    button2.remove()
 }
 
-//try to condense buttons into one function using event listener on the button
-
+//displayTurn and turn display the next piece to be played on board
 const displayTurn = () => {
     const turnTracker = document.querySelector('#turnTracker')
     while(turnTracker.firstChild) {
         turnTracker.removeChild(turnTracker.firstChild)
     }
-    
-    //combine this with addPiece function?
 }
 
 
 const turn = () => {
     displayTurn()
     const display = document.createElement('div')
-    let string24 = 'Turn: ' + currentPlayer
-    display.innerHTML = string24
+    let tracker = 'Next piece to be played: ' + currentPlayer
+    display.innerHTML = tracker
         document.querySelector('#turnTracker').appendChild(display)
     
 }
 
-
+//Adding pieces to gameboard
 const addPiece = (event) => {
-    //event in this case is click, since players are clicking on divs to place pieces
-    //need to display somewhere on page whose turn it is
-    
-    if(currentPlayer === 'X') {
-        //displayTurn()
-        
-        //looks a little wonky the way code is written now, it'd be more accurate to say most recent piece played. Can displayTurn() go somewhere else in this function?
-        /*
-        const display = document.createElement('div')
-        let string24 = 'Turn: ' + currentPlayer
-        display.innerHTML = string24
-        document.querySelector('#turnTracker').appendChild(display)
-        */
-        //piece to be placed
+    if(currentPlayer === 'X' && activeBoard === true) {
+        //adding piece to board
         event.target.classList.add('x')
         event.target.innerHTML = 'X'
         //add one to turnCounter
         turnCounter += 1
-        console.log(turnCounter)
+        //console.log(turnCounter)
+        //each div has an id that is used to calculate win logic, so need to grab that id to store in an array that can be evaluated in win logic
         const idToPush = event.target.getAttribute('id')
         playerOneArray.push(idToPush)
-        console.log(event.target.getAttribute('id'))
-        console.log(playerOneArray)
-        
+        //console.log(event.target.getAttribute('id'))
+        //console.log(playerOneArray)
         //change player
         currentPlayer = 'O'
         //prevent changing innerHTML of target div
         event.target.removeEventListener('click',addPiece)
         //display next person's turn
-        
         turn()
         winScenario()
-    } else {
-        //displayTurn()
-        
-        /*
-        const display = document.createElement('div')
-        let string24 = 'Turn: ' + currentPlayer
-        display.innerHTML = string24
-        document.querySelector('#turnTracker').appendChild(display)
-        */
+    } else if (currentPlayer === 'O' && activeBoard === true) {
         event.target.classList.add('o')
         event.target.innerHTML = 'O'
         turnCounter += 1
@@ -107,128 +75,138 @@ const addPiece = (event) => {
         playerTwoArray.push(idToPush)
         console.log(event.target.getAttribute('id'))
         console.log(playerTwoArray)
-        
         currentPlayer = 'X'
         event.target.removeEventListener('click',addPiece)
-        
         turn()
         winScenario()
     }
-    //winScenario()
 }
 
-//function to reset board
-const boardReset = (event) => {
-    playerOneArray = []
-    playerTwoArray = []
-    turnCounter = 0
+//generate board
+const initializeBoard = () => {
     const container = document.querySelector('#container')
-    while(container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
     for(let i = 1; i < 10; i++) {
         const board = document.createElement('div')
         board.classList.add('board')
         board.setAttribute('id', i)
         board.innerHTML = i
         container.appendChild(board)
+    }  
+}
+
+//reset board
+const boardReset = (event) => {
+    playerOneArray = []
+    playerTwoArray = []
+    turnCounter = 0
+    activeBoard = true
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
     }
+    initializeBoard()
+    turn()
     addListeners()
 }
 
+//adds event listeners to game board that enable pieces to be laid on board
 const addListeners = () => {
     const board = document.querySelectorAll('.board')
-    //returns array of DOM elements, will need to access elements in array
-    console.log(board)
-    
+    //console.log(board)
     for(let i = 0; i < board.length; i++) {
-        //board[i].classList.add('test')
         board[i].addEventListener('click', addPiece)
     }
-
     //add an event listener for refreshing game
     const resetButton = document.querySelector('#reset')
     resetButton.addEventListener('click', boardReset)
+
+    
 }
 
+//separated button listeners because buttons are removed once they are clicked and users are not re-prompted to enter names unless they refresh the page - restarting game doesn't refresh page
+const addNameListeners = () => {
+    //add event listener for add player 1 name button
+    const button1 = document.querySelector('#inputButton1')
+    button1.addEventListener('click', addName1)
+    //add event listener for add player 2 name button
+    const button2 = document.querySelector('#inputButton2')
+    button2.addEventListener('click', addName2)
+}
+
+//need to invert player to display winner of game
 const invertUser = () => {
     if(currentPlayer === 'O') {
-        invertedPlayer = 'X'
+        //invertedPlayer = 'X'
+        invertedPlayer = playerOne
     }
     if(currentPlayer === 'X') {
-        invertedPlayer = 'O'
+        //invertedPlayer = 'O'
+        invertedPlayer = playerTwo
     }
 }
 
+//lock board
 const disableBoard = () => {
-    //remove board class
-    const board = document.querySelectorAll('.board')
-    for(let i = 0; i < board.length; i++) {
-        //board[i].classList.add('test')
-        board[i].classList.add('hideText')
-        board[i].classList.remove('board')
-    }
+    activeBoard = false
 }
 
+//evaluate in conditions
 const winScenario = () => {
-    /*
-    if() {
-       //if 1-2-3 OR 4-5-6 OR 7-8-9 OR 1-4-7 OR 2-5-8 OR 3-6-9 OR 1-5-9 OR 7-5-3 are all populated with same type of piece, player with that piece wins 
-    } else if() {
-        alert('The game is a draw! There are no open spaces')
-    } else {
-        
-    }
-    */
-    //need to invert use because of way code is set up. Tried putting winScenario in different places within addPiece but couldn't get code to cun properly
+    //need to invert use because of way code is set up. Tried putting winScenario in different places within addPiece but couldn't get code to run properly
     invertUser()
     
     if(playerOneArray.includes('1') && playerOneArray.includes('2') &&playerOneArray.includes('3') || playerTwoArray.includes('1') && playerTwoArray.includes('2') && playerTwoArray.includes('3')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('4') && playerOneArray.includes('5') &&playerOneArray.includes('6') || playerTwoArray.includes('4') && playerTwoArray.includes('5') && playerTwoArray.includes('6')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('7') && playerOneArray.includes('8') &&playerOneArray.includes('9') || playerTwoArray.includes('7') && playerTwoArray.includes('8') && playerTwoArray.includes('9')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('1') && playerOneArray.includes('4') &&playerOneArray.includes('7') || playerTwoArray.includes('1') && playerTwoArray.includes('4') && playerTwoArray.includes('7')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('2') && playerOneArray.includes('5') &&playerOneArray.includes('8') || playerTwoArray.includes('2') && playerTwoArray.includes('5') && playerTwoArray.includes('8')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('3') && playerOneArray.includes('6') &&playerOneArray.includes('9') || playerTwoArray.includes('3') && playerTwoArray.includes('6') && playerTwoArray.includes('9')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('1') && playerOneArray.includes('5') &&playerOneArray.includes('9') || playerTwoArray.includes('1') && playerTwoArray.includes('5') && playerTwoArray.includes('9')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(playerOneArray.includes('3') && playerOneArray.includes('5') &&playerOneArray.includes('7') || playerTwoArray.includes('3') && playerTwoArray.includes('5') && playerTwoArray.includes('7')) {
-        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to keep playing.'
+        document.querySelector('#turnTracker').innerHTML = invertedPlayer + ' wins! Please hit the reset button if you want to start a new game.'
         disableBoard()
+        console.log(activeBoard)
     }
     if(turnCounter === 9) {
-    //create a div or show winning text somewhere else   
-    //alert('The game is a draw!')
-        document.querySelector('#turnTracker').innerHTML ='The game is a draw. Please hit the reset button if you want to keep playing.'
+        //game is a draw
+        document.querySelector('#turnTracker').innerHTML ='The game is a draw. Please hit the reset button if you want to start a new game.'
+        //Kept disableBoard function in this condition even though it doesn't really do anything because no open squares are left, but its intent is to "lock" the board
+        disableBoard()
+        console.log(activeBoard)
    }
 }
 
-//to clear board, need to clear innerHTML of divs with class .board
-//preventDefault to stop whole page from refreshing
 
 document.addEventListener('DOMContentLoaded',() => {
-    addName1()
-    addName2()
+    initializeBoard()
     addListeners()
-    //winScenario()
+    addNameListeners()
     turn()
 })
