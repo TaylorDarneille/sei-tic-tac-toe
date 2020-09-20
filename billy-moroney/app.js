4684f57e2c0e39f718a1cc0e5431642d1134b9f9
billymoroney <YOUR-EMAIL-ADDRESS>
2020-09-20 10:25:03 -0500

    final version before submission, needed to step away from minimax for now

--- billy-moroney/app.js
+++ billy-moroney/app.js
@@ -87,7 +87,7 @@ const pickSides = () => {
     // the player who goes first will be circle, which will be defined in markShape function
 }
 
-// function for computer to move
+//function for computer to move
 const computerMove = () =>  {
     // (9 - turnCount) allows the random number to be contained within the shrinking tile Index array
     // use a timer to make the computer choice not appear instantly
@@ -119,37 +119,142 @@ const computerMove = () =>  {
         }, 500) 
 }
 
-//function to determine whether player or computer array was tested for win
-const playerCheck = (choicesArray) => {
-    if (choicesArray === playerChoices){
-        document.querySelector('.gameInfo').innerText = 'Player Wins!'
-    } else {
-        document.querySelector('.gameInfo').innerText = 'Computer Wins!'
-    }
+// my attempt at minimax algorithm, ran out of time to work out bugs
+// const computerMove = () => {
+//     //variables
+//     let bestScore = -Infinity
+//     let move = null
+//     let depth = turnCount
+//     for (let i = 0; i < tileIndexes.length; i++){
+//         //pick next available move
+//         let choice = tileIndexes[i]
+//         //add this move to the choices array to test new board state
+//         computerChoices.push(choice + 1)
+//         //call minimax function on this board state (in my case, board state = computerChoices + playerChoices?)
+//         let score = minimax(computerChoices, playerChoices, turnCount, false)
+//         console.log(score)
+//         //remove the move that was tested so that the loop can return and run minimax on the next move choice
+//         computerChoices.pop()
+//         //keep track of best move and best score
+//         if (score > bestScore) {
+//             bestScore = score
+//             move = choice+1
+//         }
+//     }
+//     console.log(move)
+//     //make the best move
+//     if (turnCount % 2 === 0){
+//         tiles[move].innerText = 'O' 
+//     }
+//     else {
+//         tiles[move].innerText = 'X'
+//     }
+//     //remove available tile from tileIndexes array
+//     // need to randomly pick an index, than use that index to pick a tile
+//     let iD = tileIndexes.indexOf(choice)
+//     tileIndexes.splice(iD, 1)
+//     turnCount++
+//     // check for win
+//     winCheck(computerChoices) 
+//     // can move refers to the player
+//     canMove = true 
+   
+// }
 
-    //set turnCount to 9 or above to end game
-    turnCount = 10
-}
 
-//function to change style of winning line
-const winLine = (a, b, c) => {
-    if (tiles[a-1].innerText === 'X'){
-        tiles[a-1].classList.add('x')
-        tiles[b-1].classList.add('x')
-        tiles[c-1].classList.add('x')
-    } else {
-        tiles[a-1].classList.add('o')
-        tiles[b-1].classList.add('o')
-        tiles[c-1].classList.add('o')
-    }
-}
 
-//function to remove event listeners so that player cannot continue to play after a win condition is met
-const removeListeners = () => {
-    for (let i = 0; i < tiles.length; i++){
-        tiles[i].removeEventListener('click', markShape)
-    }
-}
+// const minimax = (computerChoices, playerChoices, depth, playerMove) => {
+//     //check for a winning position to send a score back up the recursive tree
+
+//     let result = null
+//     if (canMove){
+//         result = minMaxWinCheck(playerChoices)
+//     } else {
+//         result = minMaxWinCheck(computerChoices)
+//     }
+//     if (result !== null){
+//         return result - turnCount
+//     }
+//     // return 100 max - turnCount for best computer move, -100 max + turnCount for best player move
+
+//     //if it is the computer's turn (playerMove = false):
+//     //loop through the next available moves and call minimax recursively for each, assuming next move is player move
+//     if (canMove === false){
+//         let bestScore = -Infinity
+//         for (let i = 0; i < tileIndexes.length; i++){
+//             console.log(i)
+//             let choice = tileIndexes[i]
+//             computerChoices.push(choice + 1)
+//             //if it is not the computer's turn (playerMove = true):
+//             let score = minimax(computerChoices, playerChoices, true)
+//             console.log(score)
+//             computerChoices.pop()
+//             bestScore = max(score, bestScore)
+//             console.log(bestScore)
+//         }
+//     } else if (canMove === true){
+//         let choice = tileIndexes[i]
+//         playerChoices.push(choice+1)
+//         //loop through all available moves and call minimax recursively as if next turn is computer turn
+//         let score = minimax(computerchoices, playerChoices, false)
+//         playerChoices.pop()
+//         bestScore = max(score, bestScore)
+//     }
+//     //keep track of best score and return at the end
+//     return bestScore - turnCount
+// }
+
+// const minMaxWinCheck = (choicesArray) => {
+//     if (choicesArray.includes(1) && choicesArray.includes(2) && choicesArray.includes(3)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(4) && choicesArray.includes(5) && choicesArray.includes(6)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(7) && choicesArray.includes(8) && choicesArray.includes(9)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(1) && choicesArray.includes(4) && choicesArray.includes(7)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(2) && choicesArray.includes(5) && choicesArray.includes(8)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(3) && choicesArray.includes(6) && choicesArray.includes(9)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(1) && choicesArray.includes(5) && choicesArray.includes(9)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } else if (choicesArray.includes(3) && choicesArray.includes(5) && choicesArray.includes(7)) {
+//         if (choicesArray === playerChoices){
+//             return -100
+//         } else if (choicesArray === computerChoices){
+//             return 100
+//         }
+//     } 
+// }
 
 
 // function to test win conditions at the end of every move
@@ -193,12 +298,40 @@ const winCheck = (choicesArray) => {
     }
 }
 
+//function to determine whether player or computer array was tested for win
+const playerCheck = (choicesArray) => {
+    if (choicesArray === playerChoices){
+        document.querySelector('.gameInfo').innerText = 'Player Wins!'
+    } else {
+        document.querySelector('.gameInfo').innerText = 'Computer Wins!'
+    }
+
+    //set turnCount to 9 or above to end game
+    turnCount = 10
+}
+//function to change style of winning line
+const winLine = (a, b, c) => {
+    if (tiles[a-1].innerText === 'X'){
+        tiles[a-1].classList.add('x')
+        tiles[b-1].classList.add('x')
+        tiles[c-1].classList.add('x')
+    } else {
+        tiles[a-1].classList.add('o')
+        tiles[b-1].classList.add('o')
+        tiles[c-1].classList.add('o')
+    }
+}
+//function to remove event listeners so that player cannot continue to play after a win condition is met
+const removeListeners = () => {
+    for (let i = 0; i < tiles.length; i++){
+        tiles[i].removeEventListener('click', markShape)
+    }
+}
 // get reset button, allow button to reset game without refreshing page
 const resetButton = () => {
     const button = document.querySelector('button')
     button.addEventListener('click', resetGame)
 }
-
 //call back function for resetting game
 const resetGame = () => {
     // loop through tiles to remove all innertext
@@ -214,45 +347,6 @@ const resetGame = () => {
     initializeBoard()
     pickSides()
 }
-
-// Trying to incorporate min-max algorithm
-// need a way to represent game state
-const computerMove = () => {
-    //variable to keep track of best score
-    let bestScore = -Infinity
-    let move = null
-    for (let i = 0; i < 9; i++){
-        //select next available move index
-        //use that index to create a new board state
-        //pass that board state into minimax function that accepts the state, the depth, and which turn it is
-        //minimax will return a score
-        //keep track of the best move based on the best score
-        //after looping through all options, act on the best move 
-        //switch turns
-    }
-}
-
-let scores = {
-    X: 10,
-    O: -10;
-    tie: 0
-}
-
-const minimax = (board, depth, isMaximizing){
-    //first check for win
-    //if this is a winning position, return the score
-
-    //if it is the maximizing player's turn
-    //loop through all spots
-    //check for available tiles
-    //move to nearest option to create new board state
-    //call minimax recursively and pass new board state and increase depth
-    //return best score
-
-    //same implementation for the other player
-}
-
-
 document.addEventListener('DOMContentLoaded', ()=>{
     initializeBoard()
     pickSides()
