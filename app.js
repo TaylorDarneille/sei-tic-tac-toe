2639b9962eedf312e478578df9c9f3ef70d64405
epizarro98 <epizarro309@gmail.com>
2020-09-23 18:29:44 -0400

    Done but restart button doesnt work

--- app.js
+++ app.js
@@ -26,252 +26,6 @@
 // //cant click square more than once
 // //listeners for each cell
 
-// // const statusDisplay = document.querySelector('.game--status')
-
-// // let gameActive = true
-
-// // let currentPlayer = "X"
-
-// // let gameState = ["", "", "", "", "", "", "", "", ""]
-
-// // const winningMessage = () => `Player ${currentPlayer} has won!`;
-// // const drawMessage = () => `Game ended in a draw!`;
-// // const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`
-
-// // statusDisplay.innerHTML = currentPlayerTurn();
-
-
-// // const winningConditions = [
-// //     [0, 1, 2],
-// //     [3, 4, 5],
-// //     [6, 7, 8],
-// //     [0, 3, 6],
-// //     [1, 4, 7],
-// //     [2, 5, 8],
-// //     [2, 5, 8],
-// //     [0, 4, 8],
-// //     [2, 4, 6]
-// // ]
-
-// // //Updating game based on the player move, as well as update the UI based on the players move.
-// // const handleCellPlayed = (clickedCell, clickedCellIndex) => {
-// // gameState[clickedCellIndex] = currentPlayer
-// // clickedCell.innerHTML = currentPlayer
-// // }
-
-// // const handlePlayerChange = () => {
-// //     currentPlayer = currentPlayer === "X" ? "O" : "X";
-// //     statusDisplay.innerHTML = currentPlayerTurn();
-// // }
-
-// // const handleResultValidation = () => {
-// // let roundWon = false
-// // for (let i = 0; i <= 7; i++) {
-// //     const winCondition = winningConditions[i]
-// //     let a = gameState[winCondition[0]]
-// //     let b = gameState[winCondition[1]]
-// //     let c = gameState[winCondition[2]]
-// //     if (a === '' || b === '' || c === '') {
-// //         continue
-// //     }
-// //     if (a === b && b === c) {
-// //         roundWon = true 
-// //         break
-// //     }
-// // }
-// // if (roundWon){
-// //         statusDisplay.innerHTML = winningMessage()
-// //         gameActive = false
-// //         return
-// //     }
-// //     let roundDraw = !gameState.includes('')
-// //     if (roundDraw){
-// //         statusDisplay.innerHTML = drawMessage()
-// //         gameState.gameActive = false
-// //         return
-// //     }
-// //     handlePlayerChange()
-// // }
-
-// // const handleCellClick = (clickedCellEvent) => {
-// // const clickedCell = clickedCellEvent.target
-// // const clickedCellIndex = parseInt(
-// //     clickedCell.getAttribute('data-cell-index')
-// //   )
-// //   if (gameState[clickedCellIndex] !== '' || !gameActive){
-// //       return
-// //   }
-// //   handleCellPlayed(clickCell, clickedCellIndex)
-// //   handleResultValidation()
-// // }
-
-// // const handleRestartGame = () => {
-// //     gameActive = true;
-// //     currentPlayer = "X";
-// //     gameState = ["", "", "", "", "", "", "", "", ""];
-// //     statusDisplay.innerHTML = currentPlayerTurn();
-// //     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "")
-// // }
-
-// // document.querySelectorAll('.data-cell-index').forEach(cell => cell.addEventListener('click', handleCellClick));
-// // document.querySelector('.reset').addEventListener('click', handleRestartGame);
-
-
-// // const statusDisplay = document.querySelector('.game--status');
-
-// // let gameActive = true;
-// // let currentPlayer = "X";
-// // let gameState = ["", "", "", "", "", "", "", "", ""];
-
-// // const winningMessage = () => `Player ${currentPlayer} has won!`;
-// // const drawMessage = () => `Game ended in a draw!`;
-// // const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
-// // statusDisplay.innerHTML = currentPlayerTurn();
-
-// // const winningConditions = [
-// //     [0, 1, 2],
-// //     [3, 4, 5],
-// //     [6, 7, 8],
-// //     [0, 3, 6],
-// //     [1, 4, 7],
-// //     [2, 5, 8],
-// //     [0, 4, 8],
-// //     [2, 4, 6]
-// // ];
-
-// // function handleCellPlayed(clickedCell, clickedCellIndex) {
-// //     gameState[clickedCellIndex] = currentPlayer;
-// //     clickedCell.innerHTML = currentPlayer;
-// // }
-
-// // function handlePlayerChange() {
-// //     currentPlayer = currentPlayer === "X" ? "O" : "X";
-// //     statusDisplay.innerHTML = currentPlayerTurn();
-// // }
-
-// // function handleResultValidation() {
-// //     let roundWon = false;
-// //     for (let i = 0; i <= 7; i++) {
-// //         const winCondition = winningConditions[i];
-// //         let a = gameState[winCondition[0]];
-// //         let b = gameState[winCondition[1]];
-// //         let c = gameState[winCondition[2]];
-// //         if (a === '' || b === '' || c === '') {
-// //             continue;
-// //         }
-// //         if (a === b && b === c) {
-// //             roundWon = true;
-// //             break
-// //         }
-// //     }
-
-// //     if (roundWon) {
-// //         statusDisplay.innerHTML = winningMessage();
-// //         gameActive = false;
-// //         return;
-// //     }
-
-// //     let roundDraw = !gameState.includes("");
-// //     if (roundDraw) {
-// //         statusDisplay.innerHTML = drawMessage();
-// //         gameActive = false;
-// //         return;
-// //     }
-
-// //     handlePlayerChange();
-// // }
-
-// // function handleCellClick(clickedCellEvent) {
-// //     const clickedCell = clickedCellEvent.target;
-// //     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
-
-// //     if (gameState[clickedCellIndex] !== "" || !gameActive) {
-// //         return;
-// //     }
-
-// //     handleCellPlayed(clickedCell, clickedCellIndex);
-// //     handleResultValidation();
-// // }
-
-// // function handleRestartGame() {
-// //     gameActive = true;
-// //     currentPlayer = "X";
-// //     gameState = ["", "", "", "", "", "", "", "", ""];
-// //     statusDisplay.innerHTML = currentPlayerTurn();
-// //     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
-// // }
-
-// // document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
-// // document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
-
-// const startGame = () => {
-//     for(let i = 1; i <= 9; i++){
-//         clearBox(i)
-//     }
-
-//     document.turn = 'x'
-//     document.winner = null
-// }
-
-// const nextMove = (square) => {
-//     if(document.winner !== null){
-//         alert('Games over!')
-//     } else if(square.innerText == ''){
-//         square.innerText = document.turn
-//         switchTurn()
-//     } 
-// }
-
-// const switchTurn = () => {
-//     if(checkForWinner(document.turn)){
-//         alert('You win !')
-//         document.winner = document.turn
-//     }
-//     if(document.turn == 'x'){
-//         document.turn = 'o'
-//     } else {
-//         document.turn = 'x'
-//     }
-// }
-
-// const checkForWinner = (move) => {
-//     //Before the boolean the false is stating there is no winner.
-//     let result = false
-//     //winning conditions
-//     if( checkRow(1, 2, 3, move) || 
-//         checkRow(4, 5, 6, move) || 
-//         checkRow(7, 8, 9, move) || 
-//         checkRow(1, 4, 7, move) || 
-//         checkRow(2, 5, 8, move) || 
-//         checkRow(3, 6, 9, move) || 
-//         checkRow(1, 5, 9, move) || 
-//         checkRow(3, 5, 7, move)) {
-//         //Result becomes true since we have an array of winning conditions
-//         result = true
-//         }
-//         return result
-// }
-
-// const checkRow = (a, b, c, move) => {
-//     let result = false
-//     if(getBox(a) == move && getBox(b) == move && getBox(c) == move){
-//         result = true
-//     }
-//     return result
-// }
-
-// const getBox = (number) => {
-//    return document.getElementById('s' + number).innerText
-// }
-
-// const clearBox = (number) => {
-//     document.getElementById('s' + number).innerText = ''
-// }
-
-// document.addEventListener("DOMContentLoaded", startGame)
-
-
 
 
 //==============
@@ -280,8 +34,9 @@
 const resetGame = document.querySelector('.reset')
 const cellDivs = document.querySelectorAll('.cell')
 
-let player1 = 'x'
-let player2 = 'o'
+
+let currentPlayer = true
+winner = null
 
 
 let board = [
@@ -295,37 +50,107 @@ let board = [
 // })
 
 
+const gameStatus = () => {
+    const topLeft = cellDivs [0].classList[2]
+    const topMiddle = cellDivs [1].classList[2]
+    const topRight = cellDivs [2].classList[2]
+    const middleLeft = cellDivs [3].classList[2]
+    const middleMiddle = cellDivs [4].classList[2]
+    const middleRight = cellDivs [5].classList[2]
+    const bottomLeft = cellDivs [6].classList[2]
+    const bottomMiddle = cellDivs [7].classList[2]
+    const bottomRight = cellDivs [8].classList[2]
+    
+    //checking for winner
+    if(topLeft && topLeft === topMiddle && topLeft === topRight){
+        alert('Winner!')
+    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight){
+        alert('Winner!')
+    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight){
+        alert('Winner!')
+    } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft){
+        alert('Winner!')
+    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle){
+        alert('Winner')
+    } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+        alert('Winner!')
+    } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight){
+        alert('Winner')
+    } else if (topRight && topRight === middleMiddle && topRight === bottomLeft){
+        alert('Winner!')
+    } else if(topLeft && topMiddle && topRight && middleLeft && middleMiddle && 
+        middleRight && bottomLeft && bottomMiddle && bottomRight){
+        alert('Its a tie!')
+    }
+        
+    
+
+}
+
 
 const handleReset = (e) => {
-    console.log(e)
+    // console.log(e)
+    winner = null
+    for (const cellDiv of cellDivs){
+        cellDivs.classList.remove('x')
+        cellDivs.classList.remove('o')
+    }
+
 }
 resetGame.addEventListener('click', handleReset)
+
+const cellClicked = (e) => {
+    const classList = e.target.classList
+    // console.log(e.target.classList)
+    const location = classList[1]
+    console.log('location', location)
+
+    if(classList[2] === 'x' || classList[2] === 'o'){
+        return
+    }
+
+    if(currentPlayer === true){
+    classList.add('x')
+    gameStatus()
+    currentPlayer = !currentPlayer
+    } else {
+    classList.add('o')
+    gameStatus()
+    currentPlayer = !currentPlayer
+    }
+}
+
+for(let cellDiv of cellDivs){
+    cellDiv.addEventListener('click', cellClicked)
+}
+
+
 
 
 
 //adding event listener to reset button
 
+
+
 //loop through each of my cell divs and add a click event to each cell
-for(let cellDiv of cellDivs){
-    cellDiv.addEventListener('click', (e) => {
-        let span = document.createElement('span')
-        span.innerText = 'x'
-        e.target.appendChild(span)
-        console.log(cellDiv[1])
-    })
-}
 
-// for(let i = 1; i <= 8; i++){
-//     console.log(cellDivs)
-// }
-
-
-
-
-
-
-
-
+//loop attempts
+// for(let cellDiv of cellDivs){
+    //     cellDiv.addEventListener('click', (e) => {
+        //         let span = document.createElement('span')
+        //         span.innerText = 'x'
+        //         e.target.appendChild(span)
+        //         console.log(cellDiv)
+        //     }) 
+        // }
+        
+        
+        
+        
+        
+        // for(let i = 1; i <= 8; i++){
+            //     console.log(cellDivs)
+            // }
 
 
 
@@ -336,199 +161,3 @@ for(let cellDiv of cellDivs){
 
 
 
-
-
-
-
-
-
-
-
-//============================
-//paolas code
-//============================
-//              VARIABLES 
-// let moveTracker= "user"
-// let cellDivs= document.querySelectorAll(".square")
-// const resetButton= document.querySelector("#restartButton")
-// const containerDiv= document.querySelector("#container")
-// //       EVENT HANDLERS/ CALLBACK FUNCTIONS
-// const makeAMove =(e)=>{   
-//   let chosenSquare= e.target //show me the cell that was triggered  by the event
-//   console.log(chosenSquare.classList[1])// 
-//   let typeOfMove= document.createElement("p") //create a paragraph 
-//   typeOfMove.classList.add("pClass") // tried to fix a bug, so I added a class.
-//   while(!chosenSquare.firstChild){    // this means if the div hasnt been triggered do the following
-//     if (moveTracker=== "user"){  // user was defined at the top. so its going to be true, aka will give us an X.
-//       typeOfMove.innerText= "X"  //this is adding an "X" to the paragraph we created
-//       moveTracker ="user2"  //changed the value of moveTracker
-//     }else if( moveTracker=== "user2"){  //this is how we get the O
-//       typeOfMove.innerText="O"  //put the O inside the paragraph we created
-//       moveTracker="user"   //this will keep the pattern going over and over
-//     }
-//     chosenSquare.appendChild(typeOfMove)  //attach the paragraph to the triggered cell
-//   }
-//   const selectedIndex =chosenSquare.classList[1];
-//   //accessing the 2nd class of the html 
-//   ArrayofMoves(selectedIndex)
-//   }
-// /////////////////////////////////////////////////////////////////
-// const ArrayofMoves = (index) => {
-//   //we need a place to store the index of clicked cells
-//   let arr = []; //we start with an empty array
-//   for (let i = 0; i < cellDivs.length; i++) {
-//     //we need this to run through the entire grid
-//     let cell = cellDivs[i]; //short way of accesing all the cells in the grid.
-//     if (cell.querySelector("p") !== null) {
-//       //if the div is not empty aka has a p tag with inntertext than push that innertext into my array
-//       arr.push(cell.querySelector("p").innerText);
-//     } else {
-//       arr.push(null); //if the celll hasnt been clicked then push null, so i can see it
-//     }
-//     console.log(arr); //show me the array
-//     if (arr[0] !== null) {
-//       if ((arr[0] === arr[1]) == !null) {
-//         //&& arr[0]==!null) {
-//         if (arr[0] === arr[2]) {
-//           //  if (arr[0] === !null){
-//           console.log("first row");
-//           alert("winner!!!!!!!!");
-//         }
-//       }
-//     }
-//   }
-//   if (arr[3] !== null) {
-//     if ((arr[3] === arr[4]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[3] === arr[5]) {
-//         //  if (arr[0] === !null){
-//         console.log("middle row");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   }
-//   if (arr[6] !== null) {
-//     if ((arr[6] === arr[7]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[6] === arr[8]) {
-//         //  if (arr[0] === !null){
-//         console.log("bottom row");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   }
-//   if (arr[2] !== null) {
-//     if ((arr[2] === arr[5]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[2] === arr[8]) {
-//         //  if (arr[0] === !null){
-//         console.log("last column ");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   } if (arr[1] !== null) {
-//     if ((arr[1] === arr[4]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[1] === arr[7]) {
-//         //  if (arr[0] === !null){
-//         console.log("middle column ");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   } if (arr[0] !== null) {
-//     if ((arr[0] === arr[3]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[0] === arr[6]) {
-//         //  if (arr[0] === !null){
-//         console.log("first column ");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   }if (arr[0] !== null) {
-//     if ((arr[0] === arr[4]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[0] === arr[8]) {
-//         //  if (arr[0] === !null){
-//         console.log("left diagnal ");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   }if (arr[2] !== null) {
-//     if ((arr[2] === arr[4]) == !null) {
-//       //&& arr[0]==!null) {
-//       if (arr[2] === arr[6]) {
-//         //  if (arr[0] === !null){
-//         console.log("right diagnal ");
-//         alert("winner!!!!!!!!");
-//       }
-//     }
-//   }
-  
-// }
-
-
-
-// /////////////////// second winning method ////////////////////////////////////////
-
-
-//       // [0, 1, 2], done
-//       // [3, 4, 5], done
-//       // [6, 7, 8], done
-//       // [0, 3, 6], done
-//       // [1, 4, 7],done
-//       // [2, 5, 8], done 
-//       // [0, 4, 8], done
-//       // [2, 4, 6], done 
-
-// // } else if (index===0){ 
-// //   }if(arr[index]===arr[3]){
-// //       if (arr[index]===arr[6]){ 
-// //         console.log("first column horizontal")
-// //           alert("winner!!!!!!!!")
-// //       }
-// // }else if (index===1){ 
-// //   }if(arr[index]===arr[4]){
-// //     if (arr[index]===arr[7]){
-// //       console.log("middle column")
-// //         alert("winner!!!!!!!!")
-// //     }
-// // }else if (index===2){ 
-// //   }if(arr[index]===arr[5]){
-// //     if (arr[index]===arr[8]){
-// //       console.log("last column")
-// //         alert("winner!!!!!!!!")}
-// //       } 
-// //     }
-// //   }
-// // }
-// //correct code, put this on line 53 thru 66
-// // if (index===0){
-// //   //check div 1  
-// //   }if(arr[index]===arr[1]){
-// //       if (arr[index]===arr[2]){
-// //        alert("winner!!!!!!!!")
-// //   }
-// // } else if (index===0){
-// // //check div 1  
-// // }if(arr[index]===arr[3]){
-// //     if (arr[index]===arr[6]){
-// //       alert("winner!!!!!!!!")
-// //   }
-// // }
-// // }
-// const h1 = document.querySelector("h1")
-// function changeBackgroundH1 () {
-//   h1.style.background="pink"
-// }
-// h1.addEventListener("click", changeBackgroundH1)
-// ///////////////////////////////////////////////////
-// document.addEventListener('DOMContentLoaded', ()=>{ 
-//   for (let i=0; i<cellDivs.length; i++){
-//     let square= cellDivs[i]
-//     square.addEventListener("click",makeAMove)
-//   }
-// //   for (let i=0; i<cellDivs.length; i++){
-// //     let square= cellDivs[i]
-// //     square.addEventListener("click",makeAMove2)
-// //  }
-// })
