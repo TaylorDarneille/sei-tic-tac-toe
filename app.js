7caeb68558848df1d6f4c628a95e8d645249a860
adada <adewale.j.dada@gmail.com>
2020-09-23 16:15:22 -0400

    create variables to add and X or an O to a div once clicked

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-// ------------START--------------------------------------------------
-// you can find how to do this in the notes for changing the triangle
-// when a div is clicked, an X should show up
-// the following div that is clicked should show an O
-// this cycle should continue each time a div is clicked. 
+//add variable to add an x to a div when that div is clicked
+const addX = () => {
+    const x = document.createElement("p");
+    x.innerText = "X";
+    console.log(x);
+    event.target.appendChild(x);
+}
+
+//add variable to add an o to a div when that div is clicked
+const addO = () => {
+    const o = document.createElement("p");
+    o.innerText = "O";
+    console.log(o);
+    event.target.appendChild(o);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("Get Ready to Play Tic, Tac Toe!");
+
+    document.addEventListener("click", ()=>{
+        console.log(event);
+        
+
+    })
+})
  
