21f06ecfc59400ab1fe40c9fff15ad89719b2bc4
dazmag <iscope09@hotmail.com>
2020-09-22 22:52:06 -0400

    declaring the variables

---
+++ app.js
@@ -0,0 +1,9 @@
+//Gets all the squares
+const cats = document.getElementsByClassName("cat");
+
+//Variable for X
+let currentPlayer = "x";
+
+//Variable to store the game status
+let gameStatus = "Game On";
+
