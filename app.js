30ad5bc116b76f7b1c3ac94b52460104896f0315
NatrezC <cnatrez@gmail.com>
2020-09-22 22:47:43 -0500

    Reset button is functional

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 //DOM Manipulations
-// const createGrid = () => {
+// function createId(event) {
 //     for (let i = 0; i < 9; i++) {
-//         let square = document.createElement('div')
-//         square.classList.add('square')
-//         square.setAttribute('id', 'box')
-//         document.querySelector('.board').appendChild(square)
+//         const square = document.querySelectorAll('[data-cell]')
+//         //square.classList.add('square')
+//         square.setAttribute('id', i)
+        
 //     }
 // }
+
 //HTML Elements
 const whoTurn = document.querySelector('.turn');
 const resetButton = document.querySelector('.reset');
 const gameDivs = document.querySelectorAll('[data-cell]')//used data-cell to pull them for js
+const XLETTER = 'X'
+const OLETTER = 'O'
+let oTurn
 
 const playerWins = [ //ways to win the game
     [0, 1, 2],
@@ -31,28 +35,62 @@ let gameIsOn = true;
 
 
 //Functions
-    function handleClick(event) {
-        console.log('clicked')
-    }
+function handleClicks(event) {
+    
+    let square = event.target //click on certain divs
+    console.log(square.classList[1]) //grasp individual div
 
-function clickedReset(event) {
-    console.log('clicked reset')
+    //const currentClass = oTurn ? OLETTER : XLETTER //if O turn get Oletter if not get Xletter
+    //placeLetter(square, currentClass)
+    //check for win 
+    //check for draw
+    //switch turns
 }
 
+function placeLetter(square, currentClass) {
+    square.classList.add('x')
+}
 
+function clickedReset(event) {
+    startGame()
+}
+
+function startGame() {
+    reload = location.reload()
+}
 
 //event Listeners
 // document.addEventListener("DOMContentLoaded", () => {
-//     createGrid() //creates my divs for my board
+//     createId() //creates id for my square divs
+// })
+// gameDivs.forEach(square => {
+//     square.addEventListener('click', handleClicks, { once: true })
 // })
 
-gameDivs.forEach(square => {
-    square.addEventListener('click', handleClick, {once: true})
-})
-
+// gameDivs.forEach(square => {
+//     square.removeEventListener('click', handleClicks)
+// })
 
 resetButton.addEventListener('click', clickedReset)
-// gameDivs.forEach(square)
+
+
+
+///////////////////////////////////////////////////////
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < gameDivs.length; i++) {
+        let square = gameDivs[i];
+        square.addEventListener("click", handleClicks, { once:true});
+    }
+});
+
+
+
+
+//place an x or an o
+//check for win
+//check for draw
+//end game
+//
 
 
 // resetButton.addEventListener('click', () => {
