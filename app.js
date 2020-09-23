1f25467d1fccccbd3c12509b9d37a87563039064
adada <adewale.j.dada@gmail.com>
2020-09-23 14:53:18 -0400

    To-Do list added

---
+++ app.js
@@ -0,0 +1,6 @@
+// ------------START--------------------------------------------------
+// you can find how to do this in the notes for changing the triangle
+// when a div is clicked, an X should show up
+// the following div that is clicked should show an O
+// this cycle should continue each time a div is clicked. 
+ 
