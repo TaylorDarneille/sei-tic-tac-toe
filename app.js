e4b3118d899b6388bcab4a5da124e6746d3effa9
Juliana Fernandes <julianafernandes@Albertos-MacBook-Pro.local>
2020-09-23 21:00:22 -0300

    winning conditions

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const squareSeven = document.querySelector("#seven");
 const squareEight = document.querySelector("#eight");
 const squareNine = document.querySelector("#nine");
 
-// --------- divs -----------
-
-
+// --------- user -----------
+let xClicked = false
+let oClicked = false
 // ------------- leftScore and rightScore -------------
 let leftScore = document.querySelector("#scoreLeft");
 let rightScore = document.querySelector("#scoreRight");
@@ -34,6 +34,7 @@ firstScreen = () => {
       document.querySelector(".computerScoreBoard").style.display =
         "inline-block";
       document.querySelector(".userScoreBoard").style.display = "inline-block";
+      let xClicked = true
     });
     let o = document.querySelector("#o");
     o.addEventListener("click", () => {
@@ -46,6 +47,7 @@ firstScreen = () => {
       document.querySelector(".computerScoreBoard").style.display =
         "inline-block";
       document.querySelector(".userScoreBoard").style.display = "inline-block";
+      let oClicked = true;
     });
     o.addEventListener("mouseover", () => {
       o.style.height = "100px";
@@ -70,27 +72,32 @@ firstScreen = () => {
       x.style.height = "100px";
     });
   };
-  
+// const circle = () =>{
+
+// }
+// const cross = () =>{
+
+// }
 const game = () =>{
     let symbols = document.querySelectorAll(".symbol");
         for(let i = 0; i<symbols.length;i++) {
-            if (turn === "X"){
-                symbols[i].addEventListener("click",()=>{
+          symbols[i].addEventListener("click",()=>{ 
+          if (turn === "X"){
                     symbols[i].setAttribute("class","cross")
                     console.log("hey")
                     turn = "O"
-                    game()
-                })  
+                    winning()
+     
             }else if(turn === "O"){
                 console.log("get there")
-                symbols[i].addEventListener("click",()=>{
                     symbols[i].setAttribute("class","circle")
                     turn = "X"
-                    game()
-            }) 
-    }
+                    winning()   
+            }
+    })
   }
 }
+
 const restart = () =>{
   let restartButton = document.querySelector(".btn")
   restartButton.addEventListener("mouseover", ()=>{
@@ -104,6 +111,8 @@ const restart = () =>{
   restartButton.addEventListener("click",()=>{
     let circles = document.querySelectorAll(".circle")
     let cross = document.querySelectorAll(".cross")
+    document.querySelector("#container").style.display = "block";
+    document.querySelector(".alert").style.display = "none";
     for(div of circles){
       div.setAttribute("class","symbol")
     }
@@ -112,8 +121,40 @@ const restart = () =>{
     }
   })
 }
+
+const winning = () =>{
+  if(squareOne.className === "circle" && squareTwo.className === "circle" && squareThree.className === "circle" || squareFour.className === "circle" && squareFive.className === "circle" && squareSix.className === "circle" || squareSeven.className === "circle" && squareEight.className === "circle" && squareNine.className === "circle"||squareOne.className === "circle" && squareFour.className === "circle" && squareSeven.className === "circle"||squareTwo.className === "circle" && squareFive.className === "circle" && squareEight.className === "circle"|| squareThree.className === "circle" && squareSix.className === "circle" && squareNine.className === "circle"||squareOne.className === "circle" && squareFive.className === "circle" && squareNine.className === "circle"||squareThree.className === "circle" && squareFive.className === "circle" && squareSeven.className === "circle"){
+      
+      document.querySelector("#container").style.display = "none";
+      document.querySelector(".alert").style.display = "block";
+      document.querySelector(".alert").innerText = "O Wins!"
+      scoreOfLeft++
+      leftScore.innerText = scoreOfLeft
+
+  }else if (squareOne.className === "cross" && squareTwo.className === "cross" && squareThree.className === "cross" || squareFour.className === "cross" && squareFive.className === "cross" && squareSix.className === "cross" || squareSeven.className === "cross" && squareEight.className === "cross" && squareNine.className === "cross"||squareOne.className === "cross" && squareFour.className === "cross" && squareSeven.className === "cross"||squareTwo.className === "cross" && squareFive.className === "cross" && squareEight.className === "cross"|| squareThree.className === "cross" && squareSix.className === "cross" && squareNine.className === "cross"||squareOne.className === "cross" && squareFive.className === "cross" && squareNine.className === "cross"||squareThree.className === "cross" && squareFive.className === "cross" && squareSeven.className === "cross"){
+   
+    document.querySelector("#container").style.display = "none";
+    document.querySelector(".alert").style.display = "block";
+    document.querySelector(".alert").innerText = "X Wins!"
+    scoreOfRight++
+    rightScore.innerText = scoreOfRight
+  }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     firstScreen();
     game()
     restart()
+   
   });
