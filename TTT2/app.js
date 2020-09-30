d3b705139c128a0f282a24cbb12ca4d9fe39d32d
adada <adewale.j.dada@gmail.com>
2020-09-29 20:43:07 -0400

    gameboard created

--- TTT2/app.js
+++ TTT2/app.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    console.log("Stay hard");
+    const container = document.querySelector(".container");
+
+    for (let i = 0; i < 9; i++) {
+        const cellDiv = document.createElement("div");
+        cellDiv.classList.add("game-cells");
+        container.appendChild(cellDiv);
+    }
+    
+    
+    const statusBar = document.createElement("div");
+    statusBar.classList.add("status");
+    container.appendChild(statusBar);
+
+
+    const resetBtn = document.createElement("div");
+    resetBtn.classList.add("reset");
+    resetBtn.innerText = "Stay Hard Mfer"
+    container.appendChild(resetBtn);
+
+    
 })
